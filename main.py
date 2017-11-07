a76742763937eb70806ca6c024c5c221f139c955
chang <chang@technoface.co.jp>
2017-11-07 15:07:48 +0900

    edit

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import gensim 
-import mecab
+import MeCab
 import sys
 
-tagger = mecab.Tagger('-F\s%f[6] -U\s%m -E\\n')
+tagger = MeCab.Tagger('-F\s%f[6] -U\s%m -E\\n')
 
 if __name__ == "__main__":
 	lines = open("haiku.txt", "r")
-	dictionary = gensim.corpora.Dictionary(raws)
 	for line in lines:
 		result = tagger.parse(line)
 		print result[1:]
