2f88d8931e8022466ae68557bf348e3c9d6372ae
chang@technoface.co.jp <chang@technoface.co.jp>
2017-11-07 15:07:06 +0900

    edit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import gensim
 import mecab
 import sys
 
-tagger = mecab.Tagger('-F\s%f[6] -U\s%m -E\\n')
+#tagger = mecab.Tagger('-F\s%f[6] -U\s%m -E\\n')
+tagger = mecab.Tagger('-Owakati')
 
 if __name__ == "__main__":
 	lines = open("haiku.txt", "r")
