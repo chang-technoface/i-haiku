3657666725296523fb8887cc56cbfa9cc0104169
chang <chang@technoface.co.jp>
2017-11-09 18:03:39 +0900

    edit

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 #import gensim 
 import MeCab
 import sys
+import numpy as np
+import tensorflow as tf
 
 #tagger = mecab.Tagger('-F\s%f[6] -U\s%m -E\\n')
 #tagger = MeCab.Tagger('-Owakati')
 tagger = MeCab.Tagger()
 
 if __name__ == "__main__":
-	vocabulary = []
+	vocabulary = set()
 	keywords = []
+	sentences = []
+	
 	lines = open("haiku.txt", "r", encoding="utf-8")
 	for line in lines:
-		#print (line)
+		vocabulary = vocabulary.union(line)
+		sentences.append(list(line))	
 		node = tagger.parseToNode(line)
 		while node:
 			if len(node.surface) == 0:
@@ -20,10 +25,34 @@ if __name__ == "__main__":
 				continue
 			if node.feature.split(",")[0] == u"名詞":
 				keywords.append(node.surface)
-
-			vocabulary.append(node.surface)
 			node = node.next
-
-	print( keywords )
-	print( vocabulary)
 	lines.close()
+
+	#print( keywords )
+	#print( "keywords   size :" + str(len(keywords)) )
+
+	vacabulary = set(vocabulary)
+	print( vocabulary )
+	print( "vocabulary size :" + str(len(vocabulary)) )
+
+	print( sentences )
+	
+	word2int = {}
+	int2word = {}
+	for i, word in enumerate(vocabulary):
+		word2int[word] = i
+		int2word[i] = word
+	print(word2int['ん'])
+	print(int2word[10])
+
+	data = []
+	W_SIZE = 2
+	for s in sentences:
+		for i, word in enumerate(s):
+			for n in s[max(i-W_SIZE,0):min(i+W_SIZE,len(s))+1]:
+				if n != word:
+					data.append([word, n])
+	print(data)
+				
+
+		
