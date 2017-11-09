e90a1b262f9abad926757c50b729da8a77a4956b
chang <chang@technoface.co.jp>
2017-11-09 16:25:39 +0900

    edit

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
-import gensim 
+#import gensim 
 import MeCab
 import sys
 
 #tagger = mecab.Tagger('-F\s%f[6] -U\s%m -E\\n')
-tagger = mecab.Tagger('-Owakati')
+#tagger = MeCab.Tagger('-Owakati')
+tagger = MeCab.Tagger()
 
 if __name__ == "__main__":
-	lines = open("haiku.txt", "r")
+	vocabulary = []
+	keywords = []
+	lines = open("haiku.txt", "r", encoding="utf-8")
 	for line in lines:
-		result = tagger.parse(line)
-		print result[1:]
+		#print (line)
+		node = tagger.parseToNode(line)
+		while node:
+			if len(node.surface) == 0:
+				node = node.next
+				continue
+			if node.feature.split(",")[0] == u"名詞":
+				keywords.append(node.surface)
 
+			vocabulary.append(node.surface)
+			node = node.next
+
+	print( keywords )
+	print( vocabulary)
 	lines.close()
